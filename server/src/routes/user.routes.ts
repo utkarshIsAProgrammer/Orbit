ad0e3450ae4be48f67c46aab82e5e09d274f166d
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-05-20 17:38:00 +0530

    feat(server): add post bookmark collection management

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -92,3 +92,5 @@ export { router as userRoutes };
 // POST /api/users/collections - create, rename, delete bookmark folders
 
 // GET /api/users/:id/heatmap - GitHub-style contribution grid data
+
+// POST /api/users/collections - create, rename, delete bookmark folders
