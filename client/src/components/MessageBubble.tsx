bfcbdd069283b621b9d2f92f1fd7a0dbaacf76bf
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-05-20 09:06:00 +0530

    feat(client): add swipe-to-reply on message bubbles

--- client/src/components/MessageBubble.tsx
+++ client/src/components/MessageBubble.tsx
@@ -1098,3 +1098,5 @@ function VoiceNotePlayer({ url, isMe, initialDuration, disabled }: { url: string
 export default MessageBubble;
 
 // swipe right on message reveals reply button with quote preview
+
+// swipe right on message reveals reply button with quote preview
